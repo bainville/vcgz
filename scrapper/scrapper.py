aabb0821c6910483db47b924c1629da91c241ea3
Maxime Haselbauer <renn0xtek9@laposte.net>
2022-01-18 23:41:46 +0100

    Now using youtube-dl

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,44 +1,41 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 import urllib
 import json
-API_KEY="AIzaSyAnyhQ6E_ut2exthm8pHXofFrjb0DUw8dg"
-channel_id="UCk-_PEY3iC6DIGJKuoEe9bw"
+import subprocess
 
 
-# class Video:
-#     def __init__(self,url,)
+
+url = "https://www.youtube.com/c/JLMelenchon/videos"
+url = "https://www.youtube.com/watch?v=D30s3Yzb4Vc"
 
 
 class Source:
-    def __init__(self, contact_name=str, channel_id=str):
+    def __init__(self, contact_name=str, channel_url=str):
         if not contact_name.strip(""):
-            raise Exception ("contact_name empty")
-        if not channel_id.strip(""):
-            raise Exception ("channel_id empty")
-        self.contact_name=contact_name 
-        self.channel_id=channel_id
+            raise Exception("contact_name empty")
+        if not channel_url.strip(""):
+            raise Exception("channel_url empty")
+        self.contact_name = contact_name
+        self.channel_url = channel_url
 
-    def get_all_video_in_channel(channel_id):
-        base_video_url = 'https://www.youtube.com/watch?v='
-        base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+    def get_all_video_in_channel(self):
+        spc = subprocess.Popen(
+            [
+                "youtube-dl",
+                "--skip-download",
+                "--write-annotations",
+                "--write-description",
+                "--write-info-json",
+                "--no-overwrite",
+                "--download-archive",
+                "--write-info-json",
+                "--write-sub",
+                "--write-auto-sub",
+                self.channel_url,
+            ],
+            stdout=subprocess.PIPE,
+        )
+        spc.communicate()
+        spc.wait()
 
-        first_url = base_search_url+f"key={API_KEY}&channelId={self.channel_id}&part=snippet,id&order=date&maxResults=25"
-
-        video_links = []
-        url = first_url
-        while True:
-            inp = urllib.request.urlopen(url,timeout=3)
-            resp = json.load(inp)
-
-            for i in resp['items']:
-                if i['id']['kind'] == "youtube#video":
-                    video_links.append(base_video_url + i['id']['videoId'])
-
-            try:
-                next_page_token = resp['nextPageToken']
-                url = first_url + '&pageToken={}'.format(next_page_token)
-            except:
-                break
-        return video_links
-
-    def get
+    
